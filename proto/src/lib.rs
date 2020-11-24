8d5801f2ed8817d0f82c417a1e8472ad63ad3f9e
Hector Salazar <hecsalazarf@outlook.com>
2020-11-24 15:35:54 -0600

    chore(proto): Enable server and client specific code module #17

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -3,10 +3,12 @@ mod stub;
 
 pub use messages::*;
 
+#[cfg(feature = "client")]
 pub mod client {
   pub use super::stub::service::tasks_core_client::*;
 }
 
+#[cfg(feature = "server")]
 pub mod server {
   pub use super::stub::service::tasks_core_server::*;
 }
