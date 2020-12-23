f3e05183712dde81c0d773c8d9cdabbc1bc3ee6a
Hector Salazar <hecsalazarf@outlook.com>
2020-12-23 15:19:19 -0600

    refactor(embed): Move Hash utilities to TreeExt trait

--- embed/src/lib.rs
+++ embed/src/lib.rs
@@ -1,2 +1,2 @@
 pub mod list;
-pub mod hash;
+pub mod tree_ext;
--- embed/src/tree_ext.rs
+++ embed/src/tree_ext.rs
@@ -1,26 +1,32 @@
-//! A hash in a B+ tree.
+//! An extension trait which adds utility methods to Trees.
+use sled::{Batch, IVec, Result, Tree};
 
-use sled::{Batch, Db, IVec, Result, Tree};
-use std::ops::Deref;
-
-/// A hash in a B+ tree. Practically a wrapper around sled `Tree` with added
-/// functionality.
-/// `Hash` implements Deref<Target = Tree> such that a Hash acts like a Tree.
-#[derive(Debug, Clone)]
-pub struct Hash {
-  tree: Tree,
-}
-
-impl Hash {
-  /// Open a hash with the provided key.
-  pub fn open(db: &Db, key: &str) -> Result<Self> {
-    let tree = db.open_tree(key)?;
-    Ok(Self { tree })
-  }
-
+/// An extension trait which adds utility methods to Trees.
+pub trait TreeExt {
   /// A sugar function to insert many values using a sled `Batch`.
   /// Values are a slice of `(key, value)` pairs.
-  pub fn insert_many<K, V>(&self, values: &[(K, V)]) -> Result<()>
+  fn insert_many<K, V>(&self, values: &[(K, V)]) -> Result<()>
+  where
+    K: AsRef<[u8]>,
+    V: AsRef<[u8]>,
+  ;
+
+  /// Increments the number stored at key by `incr`.
+  /// If key does not exist the value is set to `incr` in a new one.
+  /// The increment argument is signed, so both increment and decrement operations
+  /// can be performed.
+  ///
+  /// Returns the value after increment, or `Error::Unsupported` if the key holds
+  /// a non `i64` value.
+  ///
+  /// This function never panics since wrapping (modular) additions and substractions are computed.
+  fn incr_by<K: AsRef<[u8]>>(&self, key: K, incr: i64) -> Result<IVec>;
+}
+
+impl TreeExt for Tree {
+  /// A sugar function to insert many values using a sled `Batch`.
+  /// Values are a slice of `(key, value)` pairs.
+  fn insert_many<K, V>(&self, values: &[(K, V)]) -> Result<()>
   where
     K: AsRef<[u8]>,
     V: AsRef<[u8]>,
@@ -41,12 +47,11 @@ impl Hash {
   /// a non `i64` value.
   ///
   /// This function never panics since wrapping (modular) additions and substractions are computed.
-  pub fn incr_by<K: AsRef<[u8]>>(&self, key: K, incr: i64) -> Result<IVec> {
+  fn incr_by<K: AsRef<[u8]>>(&self, key: K, incr: i64) -> Result<IVec> {
     use std::convert::TryInto;
 
     let mut error = false;
     let ivec = self
-      .tree
       .update_and_fetch(key, |old| {
         if let Some(val) = old {
           if let Ok(arr) = val.as_ref().try_into() {
@@ -75,30 +80,14 @@ impl Hash {
   }
 }
 
-impl Deref for Hash {
-  type Target = Tree;
-
-  fn deref(&self) -> &Tree {
-    &self.tree
-  }
-}
-
 #[cfg(test)]
 mod test {
   use super::*;
 
   #[test]
-  fn hash_deref() {
+  fn tree_ext_insert_many() {
     let db = sled::Config::new().temporary(true).open().unwrap();
-    let hash = Hash::open(&db, "my_hash").unwrap();
-
-    hash.insert("A", "aa").unwrap();
-  }
-
-  #[test]
-  fn hash_insert_many() {
-    let db = sled::Config::new().temporary(true).open().unwrap();
-    let hash = Hash::open(&db, "my_hash").unwrap();
+    let hash = db.open_tree("my_hash").unwrap();
     hash.insert_many(&[("A", "aa"), ("B", "bb"), ("C", "cc")]).unwrap();
 
     assert_eq!(Some(IVec::from("aa")), hash.get("A").unwrap());
@@ -107,9 +96,9 @@ mod test {
   }
 
   #[test]
-  fn hash_incr_by() {
+  fn tree_ext_incr_by() {
     let db = sled::Config::new().temporary(true).open().unwrap();
-    let hash = Hash::open(&db, "my_hash").unwrap();
+    let hash = db.open_tree("my_hash").unwrap();
 
     hash.insert("A", "aa").unwrap();
     // Failed to increment str
